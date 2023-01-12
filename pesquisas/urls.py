5981461125a4bb79b94cdd68301921a89264160d
ramonfrombr <ramonfrombr@gmail.com>
2023-01-11 21:51:13 -0300

    pull changes from remote

--- pesquisas/urls.py
+++ pesquisas/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
   path('', views.inicio, name='inicio'),
+  path('<int:pergunta_id>/', views.detalhe, name='detalhe'),
+  path('<int:pergunta_id>/resultados/', views.resultados, name='resultados'),
+  path('<int:pergunta_id>/votar/', views.votar, name='votar')
 ]
--- pesquisas/views.py
+++ pesquisas/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Pergunta
 
 # Create your views here.
 
-def inicio(request):
-  return HttpResponse("Olá, mundo. Você está na página inicial da pesquisa.")
+def inicio(pedido):
+  ultimas_perguntas = Pergunta.objects.order_by('-data_publicacao'[:5])
+  lista_perguntas = ', '.join([p.texto_pergunta for p in ultimas_perguntas])
+  return HttpResponse(lista_perguntas)
+
+def detalhe(pedido, pergunta_id):
+  return HttpResponse("Você está vendo a pergunta %s." % pergunta_id)
+
+def resultados(pedido, pergunta_id):
+  resposta = "Você está vendo os resultados da pergunta %s."
+  return HttpResponse(resposta % pergunta_id)
+
+def votar(pedido, pergunta_id):
+  return HttpResponse("Você está votando na pergunta %s." % pergunta_id)
