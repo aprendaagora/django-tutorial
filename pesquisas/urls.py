fe817a33ce983b385f22c8b5f62dc36359259e9a
ramonfrombr <ramonfrombr@gmail.com>
2023-01-11 22:39:02 -0300

    03-rotas

--- pesquisas/urls.py
+++ pesquisas/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'pesquisas'
 urlpatterns = [
   path('', views.inicio, name='inicio'),
-  path('<int:pergunta_id>/', views.detalhe, name='detalhe'),
+  path('<int:pergunta_id>/', views.detalhes, name='detalhes'),
   path('<int:pergunta_id>/resultados/', views.resultados, name='resultados'),
   path('<int:pergunta_id>/votar/', views.votar, name='votar')
 ]
--- pesquisas/views.py
+++ pesquisas/views.py
@@ -1,16 +1,19 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 from .models import Pergunta
 
 # Create your views here.
 
 def inicio(pedido):
-  ultimas_perguntas = Pergunta.objects.order_by('-data_publicacao'[:5])
-  lista_perguntas = ', '.join([p.texto_pergunta for p in ultimas_perguntas])
-  return HttpResponse(lista_perguntas)
+  ultimas_perguntas = Pergunta.objects.order_by("-data_publicacao")[:5]
+  contexto = {
+    'ultimas_perguntas': ultimas_perguntas
+  }
+  return render(pedido, 'pesquisas/inicio.html', contexto)
 
-def detalhe(pedido, pergunta_id):
-  return HttpResponse("Você está vendo a pergunta %s." % pergunta_id)
+def detalhes(pedido, pergunta_id):
+  pergunta = get_object_or_404(Pergunta, pk=pergunta_id)
+  return render(pedido, 'pesquisas/detalhes.html', {'pergunta': pergunta})
 
 def resultados(pedido, pergunta_id):
   resposta = "Você está vendo os resultados da pergunta %s."
